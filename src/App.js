2a8e1e240a5f156043ff0583749dabb31c24cfaa
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-05-13 17:03:09 -0500

    adding work from other repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Navbar, Collage, About, Form, Footer } from "./components/export";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Collage />
+      <About />
+      <Form />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
